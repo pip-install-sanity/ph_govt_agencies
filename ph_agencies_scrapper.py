08a4807f9b3ae5d1812497cabf9d74675e112959
Xiara Angel Barcelon <ayabarcelon@email.com>
2025-08-12 16:40:28 +0800

    Update notes in ph_agencies_scrapper.py

--- ph_agencies_scrapper.py
+++ ph_agencies_scrapper.py
@@ -4,12 +4,21 @@ import json
 
 
 url = "https://www.foi.gov.ph/agencies/"
+
+# send http get request  to the webpage
 response = requests.get(url)
+
+# parse the html
 soup = BeautifulSoup(response.text,'html.parser') # '.text' gets the full HTML content of the page as a plain string
 
+# use beautifulsoup to locate the info we want to extract
+# as checked, the information has the <script> tag with id=typesearch and type=application/json
 script_tag = soup.find("script", {"id": "typesearch", "type": "application/json"})
 
+
 agencies = json.loads(script_tag.string)
+# "script_tag.string" gets the raw json text inside the script tag
+# "json.loads" parses that string into a Python object (usually a list or dict)
 
 import csv
 
